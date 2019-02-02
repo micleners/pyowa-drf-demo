4da4a5b9832061c0fcf387303d21f67635cb5321
mbeche <mb.johnsonbece@gmail.com>
2019-02-01 18:00:38 -0600

    Create Views and URLs

--- events/views.py
+++ events/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from events.models import Event
+from events.serializers import EventSerializer
 
-# Create your views here.
+@csrf_exempt
+def event_list(request):
+    """
+    List all code events, or create a new event.
+    """
+    if request.method == 'GET':
+        events = Event.objects.all()
+        serializer = EventSerializer(events, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = EventSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+@csrf_exempt
+def event_detail(request, pk):
+    """
+    Retrieve, update or delete a code event.
+    """
+    try:
+        event = Event.objects.get(pk=pk)
+    except Event.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = EventSerializer(event)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = EventSerializer(event, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        event.delete()
+        return HttpResponse(status=204)
+
