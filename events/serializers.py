ba40096a1fd0db074f6b543f75931e74c1558367
mbeche <mb.johnsonbece@gmail.com>
2019-02-04 20:29:21 -0600

    Install Django Rest Framework JSON-API

--- events/serializers.py
+++ events/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from events.models import Event
 from django.contrib.auth.models import User
 
+
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     events = serializers.HyperlinkedRelatedField(many=True, view_name='event-detail', read_only=True)
     class Meta:
@@ -9,6 +10,12 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
         fields = ('url', 'id', 'username', 'events')
 
 class EventSerializer(serializers.HyperlinkedModelSerializer):
+    included_serializers = {
+        'coordinator': UserSerializer,
+    }
     class Meta:
         model = Event
         fields = ('url', 'id', 'title', 'presenter', 'time', 'location', 'coordinator', 'description')
+
+    class JSONAPIMeta:
+        included_resources = ['coordinator']
